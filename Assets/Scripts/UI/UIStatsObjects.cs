c11635a32a727afb965933fa891ccf0d2b47aac4
Amy S <enechan444@gmail.com>
2020-11-30 17:11:27 +0100

    Alpha Base 0.02.1
    Added Script Objects for HP, MP and DMG.

---
+++ Assets/Scripts/UI/UIStatsObjects.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UIStatsResources
+{
+    [CreateAssetMenu]
+    public class UIStatsResources : ScriptableObject
+    {
+        public Color color;
+        public int StatAmount = 1;
+
+        
+    }
+}
