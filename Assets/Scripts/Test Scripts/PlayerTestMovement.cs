023764f1dfc0c25ccb9af1a714f487a691868747
Amy S <enechan444@gmail.com>
2020-12-01 19:52:33 +0100

    Alpha Base 0.03
    Made a Quick easy test place in my Scene.
    Added a Small movement script, that is binded to the player.
    Still working on setting up UI for HP, for now.
    Will make a test script for Fire (stand in the object and take HP lose) later when I got the UI working.

---
+++ Assets/Scripts/Test Scripts/PlayerTestMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestMovement : MonoBehaviour
+{
+    public float acceleration;
+    public float maxspeed;
+
+    public Rigidbody rigidbody;
+    public KeyCode[] inputkeys;
+    private Vector3[] directionForKey;
+
+    void Start()
+    {
+        directionForKey = new Vector3[] { Vector3.forward, Vector3.back, Vector3.left, Vector3.right };
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        for (int i = 0; i < inputkeys.Length; i++) {
+            var key = inputkeys[i];
+
+            if (Input.GetKey(key)) {
+                Vector3 movment = directionForKey[i] * acceleration * Time.deltaTime;
+                movePlayer(movment);
+            }
+        }
+    }
+    void movePlayer(Vector3 movement) {
+        if (rigidbody.velocity.magnitude * acceleration > maxspeed)
+        {
+            rigidbody.AddForce(movement * -1);
+        }
+        else
+        {
+            rigidbody.AddForce(movement);
+        }
+    }
+}
---
+++ Assets/Scripts/UIStats/UILayoutSetup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UIStatsResources 
+{ 
+    public class UILayoutSetup : MonoBehaviour
+    {
+        public UIStatsResource[] uiStatResource;
+        public UILayout prefab;
+        void Start()
+        {
+            foreach (var uiStatResource in this.uiStatResource) 
+            {
+                var instance = Instantiate(this.prefab, this.transform);
+                instance.Setup(uiStatResource);
+            }
+        }
+    }
+}
