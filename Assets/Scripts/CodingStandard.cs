a47a8b85b0eb6064401391a97d2a46e3bd3b7863
dafors <da_fors@hotmail.com>
2020-12-01 10:38:51 +0100

    Add coding standard script

---
+++ Assets/Scripts/CodingStandard.cs
@@ -0,0 +1,36 @@
+﻿
+public class CodingStandard 
+{
+    // ------------ ORDERING ----------------
+    
+        // Constructor if any
+        // Variables
+            // Editor inspector variables such as Text text; 
+            // fields
+            // properties
+            // events
+
+        // public methods
+        // Monobehaviour methods
+        // private (helper) methods
+
+        private int _health = 100;
+
+        public int Health {
+            get => _health;
+            set => _health = value;
+        }
+
+
+    // ------------ CONVENTIONS ----------------
+    // only use "this" when necessary. we use _ for private fields.
+    
+    
+    
+    // private methods should be explicit for clarity. e.g:
+    private void PrivateMethods() {
+        
+        // Always use the property when accessing class fields. 
+        Health = 4; 
+    }
+}
