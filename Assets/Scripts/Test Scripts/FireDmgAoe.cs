7215015a310cbd8cc0555b200426b07ca93d9121
Amy S <enechan444@gmail.com>
2020-12-02 11:55:22 +0100

    Alpha Base 0.04
    Made so prototype for the UI that can show the UI stats that is set in the "UI Stat Resource" scriptobjects.
    Also started on a way to make hitbox for a simple fire.
    Not don't yet tho but I feel like I'm on the Right Track.
    Edits Include:
    - AmyScene
    - Prefab Player
    - Prefab Fire
    - Fire (has been added as a Prefab)
    - Prefab UILayer
    - TagManager "Added" Player as a Tag.

---
+++ Assets/Scripts/Test Scripts/FireDmgAoe.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireDmgAoe : MonoBehaviour
+{
+    public Transform fireHitboxPoint;
+    public float fireHitboxRange = 1.5f;
+    public LayerMask fireLayers;
+
+    void Update()
+    {
+        if () {
+            FireAttack();
+        }
+    }
+    void FireAttack() {
+        Collider[] hitPlayer = Physics.OverlapBox(fireHitboxPoint.position, fireHitboxRange, fireLayers);
+        int i = 0;
+        foreach (Collider fire in hitPlayer) {
+            Debug.Log("Player hit by Fire");
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (fireHitboxPoint == null)
+            return;
+        Gizmos.DrawWireSphere(fireHitboxPoint.position, fireHitboxRange);
+    }
+}
