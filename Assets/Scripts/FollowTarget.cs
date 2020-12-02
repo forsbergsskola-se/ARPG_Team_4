f788401b49e4f93ecb83f9108f6d5e0c467f1e72
Ruben Rådelius <ruben.radelius@gmail.com>
2020-12-02 11:32:31 +0100

    adds FollowTarget script to main camera

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+    public Transform myCamera;
+    public Transform target;
+    private Vector3 offset;
+
+    void Awake() {
+        offset = myCamera.position;
+    }
+
+    void Update() {
+        myCamera.position = target.position + offset;
+    }
+}
