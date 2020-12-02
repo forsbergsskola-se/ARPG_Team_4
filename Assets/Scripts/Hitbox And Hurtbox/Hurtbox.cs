12d2acac41a40c09162e6ef0f7a33ea4ca8bf874
Liam <liamthynne@gmail.com>
2020-12-02 15:56:33 +0100

    Hitbox, Hurtbox broke

--- Assets/Scripts/Hitbox And Hurtbox/Hurtbox.cs
+++ Assets/Scripts/Hitbox And Hurtbox/Hurtbox.cs
@@ -6,16 +6,18 @@ public class Hurtbox : MonoBehaviour {
     public GameObject Parent;
     
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag != HitBoxTag) return;
+        Debug.Log("Got here 1");
         
-        var thisHealthScript = this.GetComponent<ObjectHealth>();
-        var otherHitBoxScript = other.gameObject.GetComponent<Hitbox>();
-        
-        if (otherHitBoxScript.Parent == this.Parent) return;
-            
-        otherHitBoxScript.HasCollided();
-        thisHealthScript.UpdateHealth(otherHitBoxScript.Damage);
-        
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == HitBoxTag) {
+            var thisHealthScript = this.GetComponent<ObjectHealth>();
+            var otherHitBoxScript = other.gameObject.GetComponent<Hitbox>();
+            Debug.Log("Got here 2");
+            if (otherHitBoxScript.Parent == this.Parent) return;
+            Debug.Log("Got here 3");
+            otherHitBoxScript.HasCollided();
+            thisHealthScript.UpdateHealth(otherHitBoxScript.Damage);
+        }
     }
 }
 
