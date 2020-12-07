e5057d9c3119fadc0de096936b9dc750319a24a8
Liam <liamthynne@gmail.com>
2020-12-07 15:52:01 +0100

    Shooting Animaton

--- Assets/Animations/AnimationTestingLT/TriggerAnimtionTest.cs
+++ Assets/Animations/AnimationTestingLT/TriggerAnimtionTest.cs
@@ -4,6 +4,7 @@ public class TriggerAnimtionTest : MonoBehaviour {
     private Animator _triggerAnimation;
     private bool _walkingButtonPressed;
     private bool _punchButtonPressed;
+    private bool _shootButtonPressed;
 
     private void Start() {
         _triggerAnimation = GetComponent<Animator>();
@@ -12,8 +13,10 @@ public class TriggerAnimtionTest : MonoBehaviour {
     private void Update() {
         _walkingButtonPressed = Input.GetKey(KeyCode.Space);
         _punchButtonPressed = Input.GetKey(KeyCode.D);
+        _shootButtonPressed = Input.GetKey(KeyCode.F);
         
         _triggerAnimation.SetBool("IsWalking", _walkingButtonPressed);
         _triggerAnimation.SetBool("HasPunched", _punchButtonPressed);
+        _triggerAnimation.SetBool("IsShooting", _shootButtonPressed);
     }
 }
