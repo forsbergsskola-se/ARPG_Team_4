3a4910946c300f0cbe44a614f223a3d1cabd9ff8
Ruben Rådelius <ruben.radelius@gmail.com>
2020-12-08 17:52:08 +0100

    cleans up MeleeAttack script

--- Assets/Scenes/Ruben/MeleeAttack/MeleeAttack.cs
+++ Assets/Scenes/Ruben/MeleeAttack/MeleeAttack.cs
@@ -10,22 +10,21 @@ public class MeleeAttack : MonoBehaviour {
     [SerializeField] private float attacksPerSecond = 1f;
     [SerializeField] private LayerMask enemyLayers;
     private float _nextAttackTime;
-    private float _time;
     private bool _inputDisabled;
-    public int AttackDamage => attackDamage;
     private Vector3 AttackPoint => transform.TransformPoint(0, 0, attackRange);
     //TODO make character turn towards mouse target when attacking
     
     private void Update() {
-        _time = Time.time;
-        if (_inputDisabled)
+        // Use this bool to disable input if needed
+        if (_inputDisabled) 
             return;
         
-        if (Time.time >= _nextAttackTime) {
-            if (Input.GetMouseButtonDown(mouseButton)) {
-                Attack();
-                _nextAttackTime = Time.time + 1f / attacksPerSecond;
-            }
+        if (Time.time < _nextAttackTime) 
+            return;
+        
+        if (Input.GetMouseButtonDown(mouseButton)) {
+            Attack();
+            _nextAttackTime = Time.time + 1f / attacksPerSecond;
         }
     }
 
@@ -41,14 +40,4 @@ public class MeleeAttack : MonoBehaviour {
     private void OnDrawGizmosSelected() {
         Gizmos.DrawWireSphere(AttackPoint, attackRadius);
     }
-
-    // private IEnumerator SpawnAttackCollider(float time) {
-    //     var attackCollider = Instantiate(meleeHurtboxPrefab,  
-    //                                 transform.TransformPoint(0, 0, attackRange), Quaternion.identity);
-    //     attackCollider.transform.SetParent(transform);
-    //     
-    //     yield return new WaitForSeconds(time);
-    //     
-    //     Destroy(attackCollider);
-    // }
 }
