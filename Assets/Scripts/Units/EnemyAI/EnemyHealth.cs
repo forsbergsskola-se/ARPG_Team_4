0419585f8a74aa525c2763d20999a30adacd1a9f
Ruben Rådelius <ruben.radelius@gmail.com>
2020-12-08 13:15:25 +0100

    adds melee attack (WIP), adds EnemyHealth script, adds melee attack to player prefab, adds EnemyHealth to PatrollingEnemy prefab

---
+++ Assets/Scripts/Units/EnemyAI/EnemyHealth.cs
@@ -0,0 +1,21 @@
+﻿using Units;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour, IDamagable {
+    public int maxHealth = 10;
+    private int _currentHealth;
+
+    public void TakeDamage(int damage) {
+        _currentHealth -= damage;
+        
+        //If health is 0 or lower, health is set to 0 and game object is destroyed
+        if (_currentHealth <= 0) {
+            _currentHealth = 0;
+            Destroy(gameObject);
+        }
+        Debug.Log($"{name} : {_currentHealth}/{maxHealth} Health : took {damage} damage");
+    }
+    private void Awake() {
+        _currentHealth = maxHealth;
+    }
+}
