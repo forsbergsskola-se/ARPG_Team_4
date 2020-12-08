bdeaebf0626ac9f91d670db46b57233d87bd8386
Victorpetts <vike.pet@hotmail.com>
2020-12-08 18:58:43 +0100

    Chasing enemies now attack and damage player

--- Assets/Scripts/Units/EnemyAI/TrackPlayer.cs
+++ Assets/Scripts/Units/EnemyAI/TrackPlayer.cs
@@ -6,14 +6,17 @@ using UnityEngine.AI;
 namespace Units.EnemyAI {
     public class TrackPlayer : MonoBehaviour {
         private WaypointMovement waypointMovement;
+        private MeleeAttackEnemy meleeAttackEnemy;
         private NavMeshAgent _enemy;
         public GameObject player;
         public float enemyViewDistance = 7f;
         public float enemyIdleTime = 3f;
-        private bool chasing;
+        private bool _chasing;
+        private float _nextAttackTime;
 
         private void Start() {
             waypointMovement = GetComponent<WaypointMovement>();
+            meleeAttackEnemy = GetComponent<MeleeAttackEnemy>();
             if (waypointMovement == null)
                 Debug.LogWarning("Patrol point(s) not found", this);
             _enemy = GetComponent<NavMeshAgent>();
@@ -22,16 +25,22 @@ namespace Units.EnemyAI {
         private void Update() {
             var distance = Vector3.Distance(transform.position, player.transform.position);
             
-            if (chasing) {
+            if (_chasing) {
                 var enemyPos = transform.position;
                 var dirToPlayer = enemyPos - player.transform.position;
                 var newPos = enemyPos - dirToPlayer;
                 _enemy.SetDestination(newPos);
+                if (distance <= meleeAttackEnemy.attackRange) {
+                    if (Time.time < _nextAttackTime) 
+                        return;
+                    
+                    meleeAttackEnemy.Attack();
+                    _nextAttackTime = Time.time + 1f / meleeAttackEnemy.attacksPerSecond;
+                }
             }
-
             if (distance < enemyViewDistance) {
-                chasing = true;
-            } else if (chasing && distance > enemyViewDistance) {
+                _chasing = true;
+            } else if (_chasing && distance > enemyViewDistance) {
                 StartCoroutine(wait());
             } else {
                 waypointMovement.SetDestination();
@@ -42,7 +51,7 @@ namespace Units.EnemyAI {
             var enemyPos = transform.position;
             _enemy.SetDestination(enemyPos);
             yield return new WaitForSecondsRealtime(enemyIdleTime);
-            chasing = false;
+            _chasing = false;
         }
     }
 }
