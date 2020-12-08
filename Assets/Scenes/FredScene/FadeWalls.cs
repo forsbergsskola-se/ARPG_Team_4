257f0c34287b7f73b67f7d683bfd0d0a455c16f0
Fredrik Wallander <f.m.wallander@gmail.com>
2020-12-08 19:59:15 +0100

    Add fading script for walls
    Add a script that fades wall with a slider in the inspector for alpha value.
    can be added to any existing wall, or used with tags for walls with slight modifications.

---
+++ Assets/Scenes/FredScene/FadeWalls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeWalls : MonoBehaviour{
+    private UnityEngine.Camera mainCamera;
+    public GameObject player;
+    public GameObject wall;
+    private Color _color;
+    [Range(0.0f, 1.0f)]
+    public float alphaValueSlider;
+    RaycastHit hitInfo;
+    private void Start(){
+        _color = wall.GetComponent<MeshRenderer>().material.color;
+        mainCamera = UnityEngine.Camera.main;
+    }
+
+    private void Update(){
+        var dir = player.transform.position - mainCamera.transform.position;
+        if (Physics.Raycast(mainCamera.transform.position, dir, out hitInfo, 1000)){
+            if (hitInfo.collider.gameObject.CompareTag("Wall")){
+                Debug.Log("Wall Was found");
+                _color.a = alphaValueSlider;
+                wall.GetComponent<MeshRenderer>().material.color = _color;
+            }
+            else{
+                _color.a = 1;
+                wall.GetComponent<MeshRenderer>().material.color = _color;
+            }
+        }
+    }
+}
