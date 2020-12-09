70578f564399b9c4b07bbe2a6f42956e9a249c22
Liam <liamthynne@gmail.com>
2020-12-09 16:14:07 +0100

    Revert "Added Unsheathe and sheathe of gun"
    This reverts commit cab53d1d1615ab949f16bd955a76823975ff201e.

--- Assets/Animations/AnimationTestingLT/WeaponHolster.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WeaponHolster : MonoBehaviour {
-    public GameObject Holster;
-    public GameObject Hand;
-}
