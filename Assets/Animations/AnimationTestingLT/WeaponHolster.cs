cab53d1d1615ab949f16bd955a76823975ff201e
Liam <liamthynne@gmail.com>
2020-12-09 16:07:45 +0100

    Added Unsheathe and sheathe of gun

---
+++ Assets/Animations/AnimationTestingLT/WeaponHolster.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHolster : MonoBehaviour {
+    public GameObject Holster;
+    public GameObject Hand;
+}
