d45f23074d81efbfd84baf49c9dd4db3cbb9d7e2
Fredrik Wallander <f.m.wallander@gmail.com>
2020-12-14 22:54:17 +0100

    Adds stationary threat: Electrical cables
    Adds stationary threat electrical cables without floor art assets.

---
+++ Assets/Scripts/Units/EnemyAI/ElectricalRandomThreat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.VFX;
+using Random = UnityEngine.Random;
+
+namespace Units.EnemyAI{
+    public class ElectricalRandomThreat : MonoBehaviour
+    {
+        private float timer;
+        private float randomTime;
+        void Start(){
+            randomTime = Random.Range(0.5f, 2.5f);
+        }
+
+        void Update(){
+            timer += Time.deltaTime;
+            if (timer > randomTime){
+                GetComponent<VisualEffect>().Play();
+                timer -= randomTime;
+                randomTime = Random.Range(0.5f, 2.5f);
+                SendMessage("Damage", SendMessageOptions.DontRequireReceiver);
+            }
+            else{
+                SendMessage("DontDamage", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+}
