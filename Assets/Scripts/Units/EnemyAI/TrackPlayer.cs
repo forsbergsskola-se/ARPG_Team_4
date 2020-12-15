b5bcf1a1aa6eb832035492448c546b4088a17c92
Victorpetts <vike.pet@hotmail.com>
2020-12-15 12:17:26 +0100

    Tracking enemies now work properly. Re-adds textures to breakable wooden crate

--- Assets/Scripts/Units/EnemyAI/TrackPlayer.cs
+++ Assets/Scripts/Units/EnemyAI/TrackPlayer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Runtime.InteropServices.WindowsRuntime;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 // TRACKING THREAT
@@ -12,51 +10,54 @@ namespace Units.EnemyAI
         private MeleeAttackEnemy meleeAttackEnemy;
         private NavMeshAgent _enemy;
         private Transform _playerTransform;
-        public float enemyViewDistance = 7f;
-        public float enemyIdleTime = 3f;
-        private bool _chasing;
+        public float enemyViewDistance = 8f;
         private float _nextAttackTime;
 
         private void Start() {
             waypointMovement = GetComponent<WaypointMovement>();
             meleeAttackEnemy = GetComponent<MeleeAttackEnemy>();
-            
+            _enemy = GetComponent<NavMeshAgent>();
             _playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            
             if (_playerTransform == null)
                 Debug.LogError("There is no GameObject with the Player tag in the scene", this);
             
-            if (waypointMovement == null) Debug.LogWarning("Patrol point(s) not found", this);
-            _enemy = GetComponent<NavMeshAgent>();
+            if (waypointMovement == null) 
+                Debug.LogWarning("Patrol point(s) not found", this);
         }
 
         void FixedUpdate() {
             var enemyPos = transform.position;
-            var _chasing = CanSeePlayer();
             var distance = Vector3.Distance(enemyPos, _playerTransform.position);
 
-            if (_chasing) {
+            if (CanSeePlayer()) {
                 var dirToPlayer = enemyPos - _playerTransform.position;
                 var newPos = enemyPos - dirToPlayer;
-                _enemy.SetDestination(newPos);
+                    _enemy.SetDestination(newPos);
                 if (distance <= meleeAttackEnemy.attackRange) {
                     if (Time.time < _nextAttackTime) return;
-
                     meleeAttackEnemy.Attack();
                     _nextAttackTime = Time.time + 1f / meleeAttackEnemy.attacksPerSecond;
                 }
-            }
-            else {
+            } else if (PathComplete()){
                 waypointMovement.SetDestination();
             }
+        }
+        
+        private bool PathComplete() {
+            if (_enemy.remainingDistance < 1) {
+                return true;
+            }
+            return false;
         }
 
         bool CanSeePlayer() {
             RaycastHit hit;
             Vector3 rayDirection = _playerTransform.position - transform.position;
 
-            if (Vector3.Angle(rayDirection, transform.forward) <= 180 * 0.5f) {
-                // Detect if player is within the field of view
+            if (Vector3.Angle(rayDirection, transform.forward) <= 140 * 0.5f) {
                 if (Physics.Raycast(transform.position, rayDirection, out hit, enemyViewDistance)) {
+                    Debug.DrawLine(transform.position, hit.point, Color.red);
                     return hit.transform.CompareTag("Player");
                 }
             }
