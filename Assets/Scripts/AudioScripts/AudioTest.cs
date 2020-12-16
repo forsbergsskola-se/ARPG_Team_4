2581c8f982f9362e360d48f7a11fe3ff3ac0e857
Amy S <enechan444@gmail.com>
2020-12-16 11:34:07 +0100

    Audio 0.04
    Added and fixed the Button sounds for the pause menu.
    Added sound for the Bullets when they spawn.
    Edits Includes:
    - AudioTest.cs
    - Bullet.prefab
    - PauseMenuScript.cs

--- Assets/Scripts/AudioScripts/AudioTest.cs
+++ Assets/Scripts/AudioScripts/AudioTest.cs
@@ -9,27 +9,78 @@ public class AudioTest : MonoBehaviour
     public Transform TestHitBox;
     public LayerMask Layer;
     */
-    public int ButtonPress = 0;
+    public int MenuButtonPress = 0;
+    public int MenuBGMSoundOn = 0;
+    public int Menu = 0;
+    public int MenuQuitButtonPress = 0;
+
+    [FMODUnity.EventRef]
+    public string MenuBGMEvent = "";
+    FMOD.Studio.EventInstance MenuBGM;
     void Start()
     {
         //ambience = FMODUnity.RuntimeManager.CreateInstance("event:/Ambiences/Ambiences");
         //ambience.start();
-        ButtonPress = 0;
+        MenuBGMSoundOn = 0;
+        MenuButtonPress = 0;
+        MenuQuitButtonPress = 0;
     }
     void Update()
     {
-        if(ButtonPress >= 1)
+        if(MenuButtonPress >= 1)
         {
-            SoundTest();
-            ButtonPress -= 1;
+            MenuButtonSound();
+            MenuButtonPress -= 1;
         }
-        
+        if(MenuBGMSoundOn >= 1)
+        {
+            Debug.Log("Menu BGM On");
+            MenuBGMOn();
+        }
+        else
+        {
+            //Debug.Log("Menu BGM Off");
+            MenuBGMOff();
+        }
+        if(MenuQuitButtonPress >= 1)
+        {
+            MenuQuit();
+            MenuQuitButtonPress -= 1;
+        }
+        /*
+        else if (MenuBGMSoundOn == 0)
+        {
+            MenuBGMOff();
+        }
+        */
     }
-    void SoundTest() 
+    void MenuButtonSound() 
     {
+        
         FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Button/Buttons", GetComponent<Transform>().position);
     }
+    void MenuQuit()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/THESPLIT/SFXSplit/ButtonSplit/ButtonsSplit_2", GetComponent<Transform>().position);
+    }
+    void MenuBGMOn()
+    {
+        FMOD.Studio.EventInstance MenuBGM;
 
+        /*
+        MenuBGM = FMODUnity.RuntimeManager.CreateInstance("event:/THESPLIT/AmbientSplit/MenuHUMSplit");
+        //GetComponent<FMODUnity.StudioEventEmitter>().Play();
+        //FMODUnity.RuntimeManager.PlayOneShot("event:/THESPLIT/AmbientSplit/MenuHUMSplit", GetComponent<Transform>().position);
+        */
+    }
+    void MenuBGMOff()
+    {
+        GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+    }
+    void MenuPauseAudio()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("", GetComponent<Transform>().position);
+    }
         /*
         Collider[] hitPlayer = Physics.OverlapBox(TestHitBox.position, transform.localScale, Quaternion.identity, Layer);
         foreach (Collider I in hitPlayer)
